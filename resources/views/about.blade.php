e7238f9e544920abfd3ebcafd64cb53cfdcbf0ae
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2026-02-27 10:38:28 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="w-full px-6">
+            <div class="bg-white text-black p-8 rounded-lg shadow-lg">
+
+                <h1 class="text-3xl font-bold mb-6">
+                    Biodata Mahasiswa
+                </h1>
+
+                <div class="space-y-3 text-lg">
+                    <p><strong>Nama:</strong> Aini Rana Salsabil</p>
+                    <p><strong>NIM:</strong> 20240140018</p>
+                    <p><strong>Program Studi:</strong> Informatika</p>
+                    <p><strong>Hobi:</strong> Membaca</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
